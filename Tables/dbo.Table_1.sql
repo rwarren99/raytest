35463a97cbb712cdaaf70ea25e71d64ef321490e
rwarren99 <ray.warren@artsalliancemedia.com>
2017-04-10 14:32:20 +0100

    more changes

--- Tables/dbo.Table_1.sql
+++ Tables/dbo.Table_1.sql
@@ -9,7 +9,10 @@ CREATE TABLE [dbo].[Table_1]
 [col7] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
 [col8] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
 [col9] [nchar] (100) COLLATE Latin1_General_CI_AS NULL,
-[col10] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
+[col10] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[cola] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[coly] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[colc] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Table_1] ADD CONSTRAINT [PK_Table_1] PRIMARY KEY CLUSTERED  ([col1]) ON [PRIMARY]
